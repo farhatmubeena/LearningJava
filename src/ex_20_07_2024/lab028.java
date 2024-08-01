0ff0e33fe910603da7aa877b290ecd5b42537419
farhatmubeena <farhatmubeena429@gmail.com>
2024-08-01 17:05:26 +0530

    lab practice 2

---
+++ src/ex_20_07_2024/lab028.java
@@ -0,0 +1,21 @@
+package ex_20_07_2024;
+
+public class lab028 {
+    public static void main(String[] args) {
+
+        //in java we have two types of conditions if condition and switch condition
+
+        //if (condition)  ---> true or false {
+        // }
+       // else{
+
+        //}
+        int age = 13;
+        if (age > 18) {
+            System.out.println("allowed to vote");
+        }
+        else{
+            System.out.println("not allowed to vote");
+        }
+    }
+}
---
+++ src/ex_20_07_2024/lab029.java
@@ -0,0 +1,12 @@
+package ex_20_07_2024;
+
+public class lab029 {
+    public static void main(String[] args) {
+        int a = 10;
+        if (a == 4){
+            System.out.println("haha");
+        }else{
+                System.out.println("lol");
+            }
+    }
+}
